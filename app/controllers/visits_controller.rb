190defb57b84cf028b7992d7fbc128b9f7f62bf3
sonicakes <soniaparker1207@gmail.com>
2018-08-07 12:18:51 +1000

    add views for visits

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,24 +1,56 @@
 class VisitsController < ApplicationController
   before_action :authorize
   def index
-  end
-
-  def show
-    @visit = Visit.find params[:id]
-  end
-
-  def edit
-  end
-
-  def create
+    @visits = Visit.all
   end
 
   def new
+    @visit = Visit.new
   end
 
-  def destroy
+  def show
+    @visit= Visit.find params[:id]
+  end
+
+  def edit
+    @visit = Visit.find params[:id]
+    unless @current_user.visits.include? @visit
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def update
+    visit = Visit.find params[:id]
+  visit.update visit_params
+  redirect_to visit
   end
+
+  def create
+    visit = Visit.create visit_params
+    current_user.visits << visit
+    flash[:success] =  "Visit Created!"
+    redirect_to visit # GET the show pag
+  end
+
+
+
+  def destroy
+    visit = Visit.find params[:id]
+    visit.destroy if @current_user.visits.include? visit
+    flash[:success] =  "Visit has been Deleted!"
+    redirect_to visits_path
+  end
+
+  # Strong params: create a whitelist of permitted parameters
+private
+def visit_params
+  params.require(:visit).permit(:start_date, :user_id, :end_date, :location, :latitude, :longtitude)
 end
+end
+#  start_date :date
+#  end_date   :date
+#  location   :text
+#  latitude   :float
+#  longtitude :float
