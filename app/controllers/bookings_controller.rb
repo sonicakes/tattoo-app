031cdd7984b2f0a5721175df902bc94f527f6831
sonicakes <soniaparker1207@gmail.com>
2018-08-08 20:29:09 +1000

    bookings done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,9 +11,25 @@ class BookingsController < ApplicationController
   end
 
   def create
+
+    # TODO Make sure artist is not double booked.
+
+    @booking = Booking.new
+    @booking.customer_id = @current_user.id
+    @booking.date = params[:booking][:date]
+    @booking.start_hour = params[:booking][:start_hour]
+    @booking.artist_id = params[:booking][:artist_id]
+    @booking.save
   end
 
   def new
+    # Make sure that the visit_id that the user sent, actually exists
+    @visit = Visit.find params[:visit_id]
+
+    # Build an array of dates to present to the user
+    @dateRange = (@visit.start_date..@visit.end_date).to_a
+
+    @booking = Booking.new
   end
 
   def destroy
