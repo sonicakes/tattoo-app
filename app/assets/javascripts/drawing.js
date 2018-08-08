90906a892c4a4628c0cabde9bf421e22f2703316
Misael M <misaelmdev@gmail.com>
2018-08-08 20:45:03 +1000

    working on second sketch

--- app/assets/javascripts/drawing.js
+++ app/assets/javascripts/drawing.js
@@ -1,32 +1,38 @@
-// function setup() {
-//   const cnv = createCanvas(600, 600);
-//   const x = (windowWidth - width) / 2;
-//   const y = (windowHeight - height) / 2;
-//   cnv.position(x, y);
-//   resetDrawing();
-//   const button = createButton("reset").style("border", "2px solid black");
-//   button.position(700, 660);
-//   button.mousePressed(resetDrawing);
-//   const save = createButton("save").style("border", "2px solid black");
-//   save.position(800, 660);
-//   save.mousePressed(saveTattoo);
-// A
-// }
-//
-//  function resetDrawing() {
-//   background (0);
-//   strokeWeight(3);
-//   stroke(255);
-//   redraw();
-//  }
-//
-//
-// function saveTattoo() {
-//  save('myTattoo.jpg');
-//
-// }
-//
-// function touchMoved() {
-//   line(mouseX, mouseY, pmouseX, pmouseY);
-//   return false;
-// }
+$(document).ready(() => {
+if( $('body.UsersController.Usersedit').length ){
+  // code for only somecontroller.someaction here!
+function setup() {
+  const cnv = createCanvas(600, 600);
+  const x = (windowWidth - width) / 2;
+  const y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  resetDrawing();
+  const button = createButton("reset").style("border", "2px solid black");
+  button.position(700, 660);
+  button.mousePressed(resetDrawing);
+  const save = createButton("save").style("border", "2px solid black");
+  save.position(800, 660);
+  save.mousePressed(saveTattoo);
+
+}
+
+ function resetDrawing() {
+  background (0);
+  strokeWeight(3);
+  stroke(255);
+  redraw();
+ }
+
+
+function saveTattoo() {
+ save('myTattoo.jpg');
+
+}
+
+function touchMoved() {
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  return false;
+}
+
+}
+});
