2c06dfa71e09718b471b987a5d3b1aca7889ebd2
Misael M <misaelmdev@gmail.com>
2018-08-08 15:31:11 +1000

    profile fixed

---
+++ app/assets/javascripts/drawing.js
@@ -0,0 +1,32 @@
+// function setup() {
+//   const cnv = createCanvas(600, 600);
+//   const x = (windowWidth - width) / 2;
+//   const y = (windowHeight - height) / 2;
+//   cnv.position(x, y);
+//   resetDrawing();
+//   const button = createButton("reset").style("border", "2px solid black");
+//   button.position(700, 660);
+//   button.mousePressed(resetDrawing);
+//   const save = createButton("save").style("border", "2px solid black");
+//   save.position(800, 660);
+//   save.mousePressed(saveTattoo);
+// A
+// }
+//
+//  function resetDrawing() {
+//   background (0);
+//   strokeWeight(3);
+//   stroke(255);
+//   redraw();
+//  }
+//
+//
+// function saveTattoo() {
+//  save('myTattoo.jpg');
+//
+// }
+//
+// function touchMoved() {
+//   line(mouseX, mouseY, pmouseX, pmouseY);
+//   return false;
+// }
