ab3cb882bc874aa1b4497177d05195639895f008
Misael M <misaelmdev@gmail.com>
2018-08-09 16:25:00 +1000

    added before_action on application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_user
 
   def current_user
     @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
-        session[:user_id] = nil unless @current_user.present? #
+    session[:user_id] = nil unless @current_user.present? #
+    @current_user
   end
   helper_method :current_user
 
