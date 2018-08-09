34429cd10443fe972df4ca844f7708b3e28478f4
sonicakes <soniaparker1207@gmail.com>
2018-08-09 15:48:32 +1000

    fix authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
+        session[:user_id] = nil unless @current_user.present? #
   end
   helper_method :current_user
 
